9b0186ca4d75bf48a00cf1e2cfd528091fab4dae
Cereal-Mkos <65137510+Cereal-Mkos@users.noreply.github.com>
2023-01-08 20:50:01 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 PRODUCT_PACKAGES += \
